309be29ea821d2297042faf496eb4629d1179f8e
Furlos <levprimak1007@gmail.com>
2025-04-23 13:08:58 +0300

    Создание первоначальной структуры

--- telegram_bot/src/handlers/handlers.py
+++ telegram_bot/src/handlers/handlers.py
@@ -3,4 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-token = os.getenv("API_URL")
+url = os.getenv("API_URL")
+class MarzbanStates(StatesGroup):
+    waiting_for_data = State()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer(
+        "Привет! Я управляю Marzban. Введите имя и пароль админа (В виде username password):"
+    )
+@dp.message(F.text)
+async def process_direct_url(message: types.Message, state: FSMContext):
+    data = message.text.strip()
