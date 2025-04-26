353cafe48639d85d370f55b937937add2b8794fa
None <136890795+Arteems@users.noreply.github.com>
2025-04-27 01:04:58 +0200

    Delete backend/autorization directory

--- backend/autorization/service.py
+++
@@ -1,8 +0,0 @@
-class AutorizationService:
-    def __init__(self, username: str, password: str):
-        self.username = username
-        self.password = password
-
-    def autorize(self, username: str, password: str):
-        return "Token"
-        # Запрос в марзбан на авторизацию админа в начале requests(https://instant-paris.space/api/admin/token, self.username, self.password)
