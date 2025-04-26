5b10a4cbad0c9c9939861a228de1c56adc5db04f
Arteems <artm_frolov00@mail.ru>
2025-04-27 00:30:56 +0200

    добавление main с приложением fastapi и файла .gitignore

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
+
+from backend.user.api import user_router
+
+app = FastAPI()
+app.include_router(user_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3000)
