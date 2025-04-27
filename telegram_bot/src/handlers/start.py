4117fdd6be57d1d89cdfaeda59f67d995dcfe8ea
Furlos <levprimak1007@gmail.com>
2025-04-27 19:50:28 +0300

    Создание первоначальной структуры

--- telegram_bot/src/handlers/start.py
+++ telegram_bot/src/handlers/start.py
@@ -1,14 +1,10 @@
 from aiogram import Router, types
 from aiogram.filters import Command
-from src.utils.clear_chat import save_message  # Исправленный импорт
 
 start_router = Router()
 
 @start_router.message(Command("start"))
 async def cmd_start(message: types.Message):
-    bot = message.bot
-    chat_id = message.chat.id
-
     # Создаем инлайн-клавиатуру с callback-кнопками
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=[
         [
@@ -19,8 +15,7 @@ async def cmd_start(message: types.Message):
     ])
 
     # Отправляем сообщение и сохраняем его ID
-    sent_message = await message.answer(
+    await message.answer(
         "Привет! Я бот для покупки доступа к VPN!",
         reply_markup=keyboard
     )
-    await save_message(chat_id=chat_id, message_id=sent_message.message_id)
